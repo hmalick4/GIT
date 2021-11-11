cb8fc754c27e75b68d9f108bf76c33709b5a8094
hmalick4 <hira.s.malick@gmail.com>
2021-11-10 22:51:25 -0600

    Latest Code 11.10.2021

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class Constants {
+
+    public static final String CONFIGURATION_FILEPATH=System.getProperty("user.dir")+ "/src/main/resources/config.properties";
+    //copy file path of config.properties and remove everything before src so tht code is dynamic
+    //user.dir relates to each individual's computer
+    public static final int IMPLICIT_WAIT=20;
+
+
+
+}
